bf6515a954a3a7d5d1b7b32e538a9269aee51e9c
Emilio Salazar <lb3dcsh@hotmail.com>
2022-07-13 23:34:50 -0500

    correcion solicitud compra completa

--- src/main/java/ec/edu/udla/dominio/SolicitudCompraCompleta.java
+++ src/main/java/ec/edu/udla/dominio/SolicitudCompraCompleta.java
@@ -10,6 +10,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -44,6 +46,7 @@ public class SolicitudCompraCompleta implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @NotNull
     @Column(name = "idsolicitud_compra_completa")
